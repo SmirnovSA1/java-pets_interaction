d58f5e3eef758ccabfbd0e609c3f04d7ec97378b
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-02-19 22:43:34 +0300

    add all

---
+++ src/main/java/ru/yandex/practicum/controllers/CatsInteractionController.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.exceptions.IncorrectCountException;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cats")
+public class CatsInteractionController {
+    private int happiness = 0;
+
+    @GetMapping("/converse")
+    public Map<String, String> converse() {
+        happiness++;
+        return Map.of("talk", "Мяу");
+    }
+
+    @GetMapping("/pet")
+    public Map<String, String> pet(@RequestParam(required = false) final Integer count) {
+        if (count == null) {
+            throw new IncorrectCountException("Параметр count равен null.");
+        }
+        if (count <= 0) {
+            throw new IncorrectCountException("Параметр count имеет отрицательное значение.");
+        }
+
+        happiness += count;
+        return Map.of("talk", "Муррр. ".repeat(count));
+    }
+
+    @GetMapping("/happiness")
+    public Map<String, Integer> happiness() {
+        return Map.of("happiness", happiness);
+    }
+
+    @ExceptionHandler
+    public Map<String, String> handleIncorrectCount(final IncorrectCountException  e) {
+        return Map.of("error", "Ошибка с параметром count.",
+                "errorMessage", e.getMessage()
+        );
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/controllers/DogsInteractionController.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.exceptions.IncorrectCountException;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/dogs")
+public class DogsInteractionController {
+    private int happiness = 0;
+
+    @GetMapping("/converse")
+    public Map<String, String> converse() {
+        happiness += 2;
+        return Map.of("talk", "Гав!");
+    }
+
+    @GetMapping("/pet")
+    public Map<String, String> pet(@RequestParam(required = false) final Integer count) {
+        if (count == null) {
+            throw new IncorrectCountException("Параметр count равен null.");
+        }
+        if (count <= 0) {
+            throw new IncorrectCountException("Параметр count имеет отрицательное значение.");
+        }
+
+        happiness += count;
+        return Map.of("action", "Вильнул хвостом. ".repeat(count));
+    }
+
+    @GetMapping("/happiness")
+    public Map<String, Integer> happiness() {
+        return Map.of("happiness", happiness);
+    }
+
+    @ExceptionHandler
+    public Map<String, String> handleIncorrectCount(final IncorrectCountException e) {
+        return Map.of("error", "Ошибка с параметром count.",
+                "errorMessage", e.getMessage()
+        );
+    }
+}
